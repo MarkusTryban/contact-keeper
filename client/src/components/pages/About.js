ab5f1f1ac4553ad39a8d72482c98c7b031b14987
MarkusTryban <markustryban@gmail.com>
2019-08-04 18:37:18 -0400

    cleanup, add navbar & router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 0.1.0
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti culpa
+        consectetur, eaque doloremque quod ipsam enim expedita autem tempora
+        dolore suscipit laboriosam incidunt eum consequatur est reprehenderit
+        animi molestiae perspiciatis accusantium quia. Minus, porro amet!
+      </p>
+    </div>
+  );
+};
+
+export default About;
