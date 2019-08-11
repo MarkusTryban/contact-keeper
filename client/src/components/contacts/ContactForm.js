2697eb0edae1b41358a60edc4334c21151befb95
MarkusTryban <markustryban@gmail.com>
2019-08-11 19:33:53 -0400

    add updateContact to onSubmit

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -4,7 +4,7 @@ import ContactContext from '../../context/contact/contactContext';
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
 
-  const { addContact, clearCurrent, current } = contactContext;
+  const { addContact, updateContact, clearCurrent, current } = contactContext;
 
   useEffect(() => {
     if (current !== null) {
@@ -36,6 +36,7 @@ const ContactForm = () => {
     if (current === null) {
       addContact(contact);
     } else {
+      updateContact(contact);
     }
     setContact({
       name: '',
