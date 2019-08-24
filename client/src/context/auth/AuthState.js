5857497756e3b92b8d8827eb0389ff51e90f53b7
MarkusTryban <markustryban@gmail.com>
2019-08-24 16:05:17 -0400

    add register to AuthContext.Provider

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -61,7 +61,8 @@ const AuthState = props => {
         isAuthenticated: state.isAuthenticated,
         loading: state.loading,
         user: state.user,
-        error: state.error
+        error: state.error,
+        register
       }}
     >
       {props.children}
