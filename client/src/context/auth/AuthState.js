7f8170909ef5ddfde4ab2dee82d122dd3498a474
MarkusTryban <markustryban@gmail.com>
2019-08-25 16:17:34 -0400

    add loadUser functions

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -25,6 +25,20 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load user
+  const loadUser = async () => {
+    // @todo - load token into global headers
+
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register user
   const register = async formData => {
