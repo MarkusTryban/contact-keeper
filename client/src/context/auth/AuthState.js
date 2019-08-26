05fd1175615d86997b1afdeea8156b31dba5649b
MarkusTryban <markustryban@gmail.com>
2019-08-26 14:17:30 -0400

    add login function

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -66,9 +66,32 @@ const AuthState = props => {
       });
     }
   };
-  // Login user
 
-  // Logout
+  // Login user
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
+
+  // Logout user
 
   // Clear errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
