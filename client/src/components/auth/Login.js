ed243e3e06e1d2e738375a6682d4e4547e42b757
MarkusTryban <markustryban@gmail.com>
2019-08-26 14:44:30 -0400

    add setAlert if/else to onSubmit

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -32,7 +32,14 @@ const Login = props => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Login submit');
+    if (email === '' || password === '') {
+      setAlert('Please fill in all fields', 'danger');
+    } else {
+      login({
+        email,
+        password
+      });
+    }
   };
 
   return (
